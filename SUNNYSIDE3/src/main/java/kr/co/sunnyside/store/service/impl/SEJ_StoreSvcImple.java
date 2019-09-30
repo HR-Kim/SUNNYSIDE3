3bf03bc65d94ed38efc28cc850be02d6dabbdb7e
sist <sist@DESKTOP-7UE7LE3>
2019-09-30 17:46:07 +0900

    change name

---
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/store/service/impl/SEJ_StoreSvcImple.java
@@ -0,0 +1,63 @@
+package kr.co.sunnyside.store.service.impl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.sunnyside.cmn.DTO;
+import kr.co.sunnyside.cmn.SearchVO;
+import kr.co.sunnyside.store.service.SEJ_StroreSvc;
+import kr.co.sunnyside.store.service.SEJ_StroreVO;
+
+@Service
+public class SEJ_StoreSvcImple implements SEJ_StroreSvc {
+	Logger LOG = LoggerFactory.getLogger(this.getClass());
+	//dao 등록
+	@Autowired
+	private SEJ_StroreDaoImpl storeDaoImpl;
+	
+	
+	/**상품등록*/
+	@Override
+	public int do_save(DTO dto) {
+		LOG.debug("==================");
+		LOG.debug("=@Service="+dto);
+		LOG.debug("==================");
+		
+		return storeDaoImpl.do_save(dto);
+	}
+	
+	/**상품삭제*/
+	@Override
+	public int do_delete(DTO dto) {
+		
+		return storeDaoImpl.do_delete(dto);
+	}
+	
+	/**상품정보수정*/
+	@Override
+	public int do_update(DTO dto) {
+		
+		return storeDaoImpl.do_update(dto);
+	}
+	
+	/**상품선택*/
+	@Override
+	public DTO get_selectOne(DTO dto) {
+		
+		return storeDaoImpl.get_selectOne(dto);
+	}
+	
+	/**상품전체조회*/
+	@Override
+	public List<?> get_retrieve(DTO dto) {
+		
+		return storeDaoImpl.get_retrieve(dto);
+	}
+
+
+
+}
--- SUNNYSIDE3/src/main/java/kr/co/sunnyside/store/service/impl/SEJ_StroreSvcImple.java
+++
@@ -1,7 +0,0 @@
-package kr.co.sunnyside.store.service.impl;
-
-import kr.co.sunnyside.store.service.SEJ_StroreSvc;
-
-public class SEJ_StroreSvcImple implements SEJ_StroreSvc {
-
-}
