4d2913eab1e3fb83360b5fcb0ce843911165db4d
sist <sist@DESKTOP-CQD7QRM>
2019-10-08 17:36:46 +0900

    PlanedDao Test

---
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/movie/service/impl/LHJ_PlanedDaoImpl.java
@@ -0,0 +1,131 @@
+package kr.co.sunnyside.movie.service.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.sunnyside.cmn.DTO;
+import kr.co.sunnyside.cmn.SearchVO;
+import kr.co.sunnyside.cmn.WorkDiv;
+import kr.co.sunnyside.movie.service.LHJ_MovieVO;
+
+@Repository
+public class LHJ_PlanedDaoImpl implements WorkDiv {
+	Logger LOG=LoggerFactory.getLogger(this.getClass());
+	   
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	      
+	private final String NAMESPACE = "kr.co.sunnyside.planed";
+	
+	public int do_update_planedToScreen(DTO dto) {
+		String statement = NAMESPACE + ".do_update_planedToScreen";
+		LHJ_MovieVO inVO = (LHJ_MovieVO) dto;
+		LOG.debug("=============================");
+		LOG.debug("1. param:"+inVO);
+		LOG.debug("=============================");
+		
+		LOG.debug("=============================");
+		LOG.debug("2. statement:"+statement);
+		LOG.debug("=============================");
+		
+		int flag = this.sqlSessionTemplate.update(statement, inVO);		
+		LOG.debug("=============================");
+		LOG.debug("3. flag:"+flag);
+		LOG.debug("=============================");
+		return flag;
+	}
+	
+	public int do_update_planedDown(DTO dto) {
+		String statement = NAMESPACE + ".do_update_planedDown";
+		LHJ_MovieVO inVO = (LHJ_MovieVO) dto;
+		LOG.debug("=============================");
+		LOG.debug("1. param:"+inVO);
+		LOG.debug("=============================");
+		
+		LOG.debug("=============================");
+		LOG.debug("2. statement:"+statement);
+		LOG.debug("=============================");
+		
+		int flag = this.sqlSessionTemplate.update(statement, inVO);		
+		LOG.debug("=============================");
+		LOG.debug("3. flag:"+flag);
+		LOG.debug("=============================");
+		return flag;
+	}
+	
+	public int do_update_planedUp(DTO dto) {
+		String statement = NAMESPACE + ".do_update_planedUp";
+		LHJ_MovieVO inVO = (LHJ_MovieVO) dto;
+		LOG.debug("=============================");
+		LOG.debug("1. param:"+inVO);
+		LOG.debug("=============================");
+		
+		LOG.debug("=============================");
+		LOG.debug("2. statement:"+statement);
+		LOG.debug("=============================");
+		
+		int flag = this.sqlSessionTemplate.update(statement, inVO);		
+		LOG.debug("=============================");
+		LOG.debug("3. flag:"+flag);
+		LOG.debug("=============================");
+		return flag;
+	}
+	
+	@Override
+	public int do_update(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int do_delete(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int do_save(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DTO do_selectOne(DTO dto) {
+		String statement = NAMESPACE + ".do_selectOne";
+		LHJ_MovieVO inVO = (LHJ_MovieVO) dto;
+		LOG.debug("=============================");
+		LOG.debug("1. param:"+inVO);
+		LOG.debug("2. statement:"+statement);
+		LHJ_MovieVO outVO = this.sqlSessionTemplate.selectOne(statement, inVO);		
+		LOG.debug("3. outVO:"+outVO);
+		LOG.debug("=============================");
+		
+		return outVO;
+	}
+
+	@Override
+	public List<?> do_retrieve(DTO dto) {
+		String statement = NAMESPACE + ".do_retrieve";
+		SearchVO search = (SearchVO) dto;
+		LOG.debug("=============================");
+		LOG.debug("1. param:"+search);
+		LOG.debug("2. statement:"+statement);
+		List<LHJ_MovieVO> list = this.sqlSessionTemplate.selectList(statement, search);		
+		LOG.debug("3. list:"+list);
+		LOG.debug("=============================");
+		
+		return list;
+	}
+
+	@Override
+	public List<?> do_excelDown(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
