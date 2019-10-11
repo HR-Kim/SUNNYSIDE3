ff1ac31bfff6d3b54f5837eed9ab237f8358a532
alhard01 <alhard01@gmail.com>
2019-10-11 17:42:24 +0900

    오류수정

--- SUNNYSIDE3/src/main/java/kr/co/sunnyside/reservation/service/impl/LGS_ReservationSvcImpl.java
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/reservation/service/impl/LGS_ReservationSvcImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.co.sunnyside.cmn.DTO;
 import kr.co.sunnyside.reservation.service.LGS_ReservationSvc;
 
+@Service
 public class LGS_ReservationSvcImpl implements LGS_ReservationSvc {
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 	
