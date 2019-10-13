5c83abd7f07c6b8288346fac266761bb95218540
JUN <JUN@DESKTOP-AIMH5TK>
2019-10-13 16:16:28 +0900

    main 틀

--- SUNNYSIDE3/src/main/java/kr/co/sunnyside/main/web/LHJ_MainCtrl.java
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/main/web/LHJ_MainCtrl.java
@@ -1,5 +1,22 @@
 package kr.co.sunnyside.main.web;
 
-public class LHJ_MainCtrl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.sunnyside.movie.service.LHJ_MovieVO;
+
+@Controller
+public class LHJ_MainCtrl {
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
+	/**단건조회 */
+	@RequestMapping(value="main/main.do",method = RequestMethod.GET)
+	public String do_selectOne(LHJ_MovieVO inVO,Model model) {
+	 
+	 return "main/main";
+	}
 }
---
+++ SUNNYSIDE3/src/main/webapp/main/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div  style="height: 800px; width: 100%; border: 5px solid #3A0729; text-align:center;">
+		<h1><b>메인 예정</b></h1>
+	</div>
+</body>
+</html>
