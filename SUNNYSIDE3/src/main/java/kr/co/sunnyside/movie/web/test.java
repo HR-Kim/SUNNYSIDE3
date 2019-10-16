4e5ffcd4f96c4cb7e146867feb328327b93e3aac
sist <sist@DESKTOP-CQD7QRM>
2019-10-16 17:05:32 +0900

    템플릿 리소스

---
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/movie/web/test.java
@@ -0,0 +1,33 @@
+package kr.co.sunnyside.movie.web;
+
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class test {
+
+	public static void main(String[] args) {
+//		Date date = new Date();
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//		String todayDate = sdf.format(date);
+//		System.out.println(todayDate);
+		
+//		Calendar cal = new GregorianCalendar();
+//		cal.add(Calendar.DATE, -1);
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//		String yesterdayDate = sdf.format(cal.getTime());
+//		
+//		System.out.println(yesterdayDate);
+//		String str = "몬스터 하우스";
+//		str = str.split(" ")[0].trim();
+//		System.out.println(str+" ");
+		
+		String one = "1";
+		String ten = "10";
+		
+		System.out.println(ten.length());
+	}
+
+}
