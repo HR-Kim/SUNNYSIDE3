e2e51e076046a946966e50cf83327f46af99468a
sist <sist@DESKTOP-CQD7QRM>
2019-10-24 20:58:20 +0900

    자주하는 질문 등록/수정/삭제/조회

--- SUNNYSIDE3/src/main/java/kr/co/sunnyside/faq/service/impl/LHJ_FaqSvcImpl.java
+++ SUNNYSIDE3/src/main/java/kr/co/sunnyside/faq/service/impl/LHJ_FaqSvcImpl.java
@@ -1,10 +1,40 @@
 package kr.co.sunnyside.faq.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.sunnyside.cmn.DTO;
 import kr.co.sunnyside.faq.service.LHJ_FaqSvc;
 
 @Service
 public class LHJ_FaqSvcImpl implements LHJ_FaqSvc {
+	@Autowired
+	LHJ_FaqDaoImpl faqDaoImpl;
+	
+	/**수정*/
+	@Override
+	public int do_update(DTO dto) {
+		return faqDaoImpl.do_update(dto);
+	}
+
+	/**삭제*/
+	@Override
+	public int do_delete(DTO dto) {
+		return faqDaoImpl.do_delete(dto);
+	}
+
+	/**저장*/
+	@Override
+	public int do_save(DTO dto) {
+		return faqDaoImpl.do_save(dto);
+	}
+
+	/**전체조회*/
+	@Override
+	public List<?> do_retrieve() {
+		return faqDaoImpl.do_retrieve();
+	}
 	
 }
